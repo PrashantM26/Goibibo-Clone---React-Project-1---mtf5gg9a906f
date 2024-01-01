96578ce2f42965777ee852ced92d115f37376d0b
PrashantM26 <mishraprashant708@gmail.com>
2024-01-01 20:16:01 +0530

    Probably final

--- src/components/navbar/nav-comp/Authentication/AuthNavigator.jsx
+++ src/components/navbar/nav-comp/Authentication/AuthNavigator.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "./AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
 export const AuthNavigator = ({ children }) => {
   const { isLoggedIn } = useAuth();
   const { pathname } = useLocation();
-  console.log("isLoggedIn:", isLoggedIn);
-  return isLoggedIn ? (
+  
+ /* return isLoggedIn ? (
     children
   ) : (
     <>
-      {/*<Navigate to="/login" state={{ prevPath: pathname }} />*/}
+      {/*<Navigate to="/login" state={{ prevPath: pathname }} />*}
       {alert("Please log in")}
     </>
   );
+};*/
+
+useEffect(() => {
+  if (!isLoggedIn) {
+    alert("Please log in");
+  }
+}, [isLoggedIn, pathname]);
+
+  return isLoggedIn ? children : null;
 };
