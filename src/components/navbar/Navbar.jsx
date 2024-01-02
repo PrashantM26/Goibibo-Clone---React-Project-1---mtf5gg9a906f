ef63ae0107e3000d791939a9f8ce18cc73bd85fa
PrashantM26 <mishraprashant708@gmail.com>
2024-01-02 19:30:59 +0530

    Probably final

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -91,14 +91,13 @@ export const Navbar = () => {
                             </div></span><div className="tooltip gr-grey-text--dark">
                             <p>Access your bookings, easy cancellation, date change and much more</p>
                         </div></div>
-                <div className="login__tab gotrible" role="presentation" style={{position: "relative"}}>
+                <div className="login__tab gotrible" role="presentation" style={{position: "relative"}} onClick={()=>{
+                    setShow(!show);
+                }}>
                     <span className="header-sprite user-icon gr-append-right5">
                     </span>
                     <div className="gr-font10" role="presentation" id="get_sign_in">
-                        <p className="gr-cap-text gr-blue-text gr-bold" onClick={()=>{
-                            console.log("clicked")
-                            setShow(!show);
-                        }} >
+                        <p className="gr-cap-text gr-blue-text gr-bold" >
                             {userLoginName}
                         </p>
                     </div>
